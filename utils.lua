b30fa45f25cedbda3bf9dfe85f751a3ca4e7a70b
Andrew Apted <ajapted@users.sf.net>
2015-03-12 16:01:54 +1100

    Added 'utils.lua' file for general utility functions.
    So far it just contains geom.ang_to_vec()

---
+++ utils.lua
@@ -0,0 +1,16 @@
+--
+--  Utility functions
+--
+
+
+geom = {}
+
+function geom.ang_to_vec(angle, len)
+  len = len or 1
+
+  local dx =  math.cos(angle * math.pi / 180.0)
+  local dy = -math.sin(angle * math.pi / 180.0)
+
+  return dx * len , dy * len
+end
+
